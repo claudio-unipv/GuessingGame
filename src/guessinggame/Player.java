3ba72202076d289b13e6badf270d64f102b617a6
Claudio Cusano <claudio.cusano@unipv.it>
2016-03-06 14:31:00 +0100

    Removed some useless comments.

--- src/guessinggame/Player.java
+++ src/guessinggame/Player.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package guessinggame;
 
 
 /**
- * A Player of the guessing game.
+ * An abstract player of the guessing game.
  * 
  * @author Claudio Cusano
  */

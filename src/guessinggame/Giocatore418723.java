57c48e53267ceb0eba86835bd6cbe1ec3e09fb98
Yoga <Yoga@DESKTOP-VHDGSJM>
2016-03-08 17:58:53 +0100

    yolo

--- src/guessinggame/Giocatore418723.java
+++ src/guessinggame/Giocatore418723.java
@@ -20,15 +20,16 @@ public class Giocatore418723 extends Player {
     }
 
     public int chooseSecretNumber() {
-        if (numero == 0) {
+      
+        if (numero == 2) {
+            return 10;
+        }
+        if (numero == 3) {
+            return 9;
+        }else {
             return 9;
         }
-        if (numero == 1) {
-            return 10;
-        } else {
-            return 0;
-
-        }
+    
 
 
     }

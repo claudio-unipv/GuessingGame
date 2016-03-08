ec9c84f38685baedc88a9541b9157b4247647bce
cl419842 <cl419842@aga20>
2016-03-08 17:16:29 +0100

    small changes pt.2

--- src/guessinggame/Player419842.java
+++ src/guessinggame/Player419842.java
@@ -14,7 +14,7 @@ import java.util.Random;
 public class Player419842 extends Player{
     
     public int to_guess;
-    
+    public int number_to_avoid;
 
     public Player419842() {
         super("Luca", "419842");
@@ -24,7 +24,15 @@ public class Player419842 extends Player{
     public int chooseSecretNumber() {
         int[] arr = {2,1};
         int rnd = new Random().nextInt(arr.length);
-        return arr[rnd];
+        do
+        {
+            rnd = new Random().nextInt(arr.length);
+            System.out.println("evito..." +number_to_avoid);
+            if (number_to_avoid==0){
+                number_to_avoid=1;
+            }
+        }while(number_to_avoid==rnd);
+        return rnd;
     }
 
     @Override
@@ -32,21 +40,30 @@ public class Player419842 extends Player{
         if(to_guess==0){
             to_guess=10;
         }
-        System.out.println(to_guess);
+        System.out.println("indovino..."+to_guess);
         return to_guess;
     }
     
     @Override
     public void lastRoundStatistics(int[] secretCounts, int guessCounts[]) {
-        int max = 0;
+        int max1 = 0;
+        int max2 = 0;
         int number_to_guess=10;
         for (int i=0;i<secretCounts.length;i++){
-            if (secretCounts[i]>max){
-                max=secretCounts[i];
+            if (secretCounts[i]>max1){
+                max1=secretCounts[i];
                 number_to_guess = i;
             }
         }
-        to_guess = number_to_guess;   
+        
+        to_guess = number_to_guess;
+        
+        for (int i=0;i<guessCounts.length;i++){
+            if (guessCounts[i]>max2){
+                max2=guessCounts[i];
+                number_to_avoid = i;
+            }
+        }
     }   
     
 }

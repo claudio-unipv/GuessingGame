23729fbcb141a115eb6bd17ba14936ce75e850a1
cl421357 <cl421357@aga42>
2016-03-08 15:50:16 +0100

    Tolto un giocatore duplicato

--- src/guessinggame/Giocatore421357.java
+++ src/guessinggame/Giocatore421357.java
@@ -22,30 +22,10 @@ public class Giocatore421357 extends Player {
 
     @Override
     public int guessNumber() {
-       return 10;
+       return 8;
     }
     
-    @Override
-    public void lastRoundStatistics(int[] secretCounts, int guessCounts[]) {
-        int medias=0;
-        int mediag=0;
-        System.out.println("LISTA CODICI SEGRETI");
-        for(int i=0;i<secretCounts.length;i++){
-            
-            System.out.println(secretCounts[i]);
-            medias+=secretCounts[i];
-        }
-        System.out.println("LISTA CODICI SEGRETI IPOTIZZATI DA ALTRI"); 
-        for(int i=0;i<guessCounts.length;i++){
-            System.out.println(guessCounts[i]);
-            mediag+=guessCounts[i];
-        }
-        
-        System.out.println("MEDIA CODICI SEGRETI"+medias/(secretCounts.length-1));
-        System.out.println("LISTA CODICI SEGRETI IPOTIZZATI DA ALTRI"+mediag/(guessCounts.length-1));
-            
-        
-    }  
+    
     
     
         

64e46ecc0c10269b3fb24cbd7782f12ea146355e
Andrea <Andrea@10.65.9.194>
2016-03-08 15:44:33 +0100

    Aggiunto giocatore

---
+++ src/guessinggame/Varano411161.java
@@ -0,0 +1,29 @@
+package guessinggame;
+
+/**
+ *
+ * @author Andrea
+ */
+public class Varano411161 extends Player {
+    
+    public Varano411161(){
+        super("Varano", "411161");       
+    }
+    
+    @Override
+    public int chooseSecretNumber(){
+    
+    return 4;
+    }
+ 
+    
+    @Override
+    public int guessNumber()
+    {
+        return 7;
+    }
+}
+
+
+    
+

b24bf53a23428ed7eed95dbc337b1800b54298b9
utente <utente@169.254.91.46>
2016-03-09 15:58:54 +0100

    fix guess strategy

--- src/guessinggame/Player419842.java
+++ src/guessinggame/Player419842.java
@@ -13,8 +13,11 @@ import java.util.Random;
  */
 public class Player419842 extends Player{
     
-    public int to_guess;
-    public int number_to_avoid;
+    private int number_to_guess;
+    private int number_to_avoid;
+    private int number_to_choose;
+    private int points;
+    private int round=1;
 
     public Player419842() {
         super("Luca", "419842");
@@ -22,46 +25,40 @@ public class Player419842 extends Player{
 
     @Override
     public int chooseSecretNumber() {
-        int[] arr = {1,2};
-        int rnd = new Random().nextInt(arr.length);
-        do
-        {
-            rnd = new Random().nextInt(arr.length);
-            if (number_to_avoid==0){
-                number_to_avoid=2;
-            }
-        }while(number_to_avoid==rnd);
-        return rnd;
+        if(number_to_choose==0){
+            number_to_choose=1;
+        }
+        return number_to_choose;
     }
 
     @Override
     public int guessNumber() {
-        if(to_guess==0){
-            to_guess=10;
+        if(number_to_guess==0){
+            number_to_guess=6;
         }
-        return to_guess;
+        return number_to_guess;
     }
     
     @Override
     public void lastRoundStatistics(int[] secretCounts, int guessCounts[]) {
-        int max1 = 0;
-        int max2 = 0;
-        int number_to_guess=10;
-        for (int i=0;i<secretCounts.length;i++){
-            if (secretCounts[i]>max1){
-                max1=secretCounts[i];
-                number_to_guess = i;
+        
+        int punteggio1 = 0;
+        
+        for (int i=1;i<secretCounts.length;i++){
+            int pts = i*secretCounts[i];
+            if(punteggio1<pts){
+                punteggio1=pts;
+                number_to_guess=i;
             }
         }
         
-        to_guess = number_to_guess;
-        
-        for (int i=0;i<guessCounts.length;i++){
-            if (guessCounts[i]>max2){
-                max2=guessCounts[i];
-                number_to_avoid = i;
+        for (int i=1;i<guessCounts.length;i++){
+            int pts = i*guessCounts[i];
+            if(punteggio1>pts || secretCounts[i]==0){
+                number_to_choose = i;
+                punteggio1=pts;
             }
         }
-    }   
-    
+        round++;
+    }
 }
